01895b084868baca59d755b6daed7b3ee0b391a2
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2023-09-04 16:14:39 +0200

    Create a test for items with a specified title

--- src/test/java/com/example/TradingApp/model/ItemsTest.java
+++ src/test/java/com/example/TradingApp/model/ItemsTest.java
@@ -7,10 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ItemsTest {
     @Test
-    void constructorCreateListOfItemsOnMarket(){
+    void constructorCreateListOfNewestItemsOnMarket(){
         Items items = new Items(DMarketService.getItemsFromMarket());
         items.printItems();
         assertTrue(items.items.size()>49);
+
+    }
+
+    @Test
+    void constructorCreateListOfSpecifiedItemsOnMarket(){
+        Items newestItems = new Items(DMarketService.getItemsFromMarket());
+
+        Item bestDiscountItem = newestItems.items.get(0);
+
+        System.out.println(bestDiscountItem.title() + bestDiscountItem.discount());
+
+        Items items = new Items(DMarketService.getOffersByTitle(bestDiscountItem.title()));
+
+       // System.out.println(DMarketService.getOffersByTitle("AWP | Exoskeleton (Field-Tested) "));
+        items.items = Items.removeWrongItemTitle(items.items, bestDiscountItem.title());
+
+        items.printItems();
+        assertTrue(items.items.size()>1);
 
     }
 
