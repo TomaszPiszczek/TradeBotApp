a6efd4a58edbd977bc1fcd3d5b5fe17271885dd8
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2023-09-05 13:14:00 +0200

    change price to double

--- src/main/java/com/example/TradingApp/model/Item.java
+++ src/main/java/com/example/TradingApp/model/Item.java
@@ -1,4 +1,4 @@
 package com.example.TradingApp.model;
 
-public record Item(String title,boolean inMarket,String status,int price,String currency,int discount,String offerId,String type,String category) {
+public record Item(String title, boolean inMarket, String status, double price, String currency, int discount, String offerId, String type, String category) {
 }
